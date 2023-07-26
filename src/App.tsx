119cf5da268485e3368440a51460db9f3d09bc84
Namasivaayam L <namasivaayam007vijay@gmail.com>
2023-07-26 11:50:22 +0530

    Removed Multi Axis Chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { GrpBar } from './components/charts/groupedBar'
-import { MultiAxis } from './components/charts/multiAxisLine'
 import { Hybrid } from './components/charts/hybrid'
 
 function App() {
 
   return (
-    <div style={{height:'90vh',width:'90vw'}}>
-      {/* <GrpBar /> */}
+    <div style={{ height: '90vh', width: '90vw' }}>
+      <GrpBar /> <br /><br /><br />
       <Hybrid/>
     </div>
   )
